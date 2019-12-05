c43ab522994161c23104d6b69297a01b151f91fb
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2019-12-05 19:49:52 +0300

    Structure created and userController added
    Eslint and .env added.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/userRouter');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
