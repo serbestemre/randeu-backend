94151334440b74897d72a145a086b4b5e6616a96
Emre Serbest <srbstemre@gmail.com>
2020-03-11 23:16:07 +0300

    assignService price and duration bug fixed

--- src/dataAccess/Business.js
+++ src/dataAccess/Business.js
@@ -1,13 +1,9 @@
 const Business = require('../models/Business');
 
-exports.findBusinessByIdDB = async businessId => Business.findById(businessId).lean();
+exports.findBusinessByIdDB = async businessId => Business.findById(businessId);
 
 exports.deleteOneDB = async business => Business.deleteOne(business);
 
 exports.updateServiceListDB = async (_id, serviceList) => {
   Business.updateOne({ _id }, { $set: { serviceList } });
-};
-
-exports.updateProvidingServicesListDB = async (_id, service) => {
-  Business.updateOne({ _id }, { $push: { 'employeeList.providingServices': service } });
 };
