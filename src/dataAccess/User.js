acbe3d848936f2e1f64f9f63c23324f3534d54b6
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2020-03-11 16:12:05 +0300

    User accessLayer bug fixed

--- src/dataAccess/User.js
+++ src/dataAccess/User.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-exports.findUserByIdDB = async userId => User.findById(userId);
+exports.findUserByEmailDB = async email => User.findOne({ email });
 
 exports.updateUserRoles = async (_id, roles) =>
   User.updateOne({ _id }, { $set: { roles } });
