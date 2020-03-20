0813f6612d93e405117cbdcf3be5c21632c9bd43
Emre Serbest <srbstemre@gmail.com>
2020-03-20 20:38:52 +0300

    roleChecker middleware implemented.

--- src/middleware/roleChecker.js
+++ src/middleware/roleChecker.js
@@ -1,10 +1,14 @@
 const Response = require("../helpers/response");
 const CommonError = require("../errors/CommonError");
+const CONSTANTS = require("../constants");
 
 function roleCheck(allowedRoles) {
   return function(req, res, next) {
     try {
       const { roles } = req.userData;
+      if (roles.includes(CONSTANTS.ROLES.SUPER_USER))
+        next();
+      else
       if (allowedRoles.some(role => roles.includes(role))) next();
       else Response.withError(res, CommonError.notAuthorized());
     } catch (err) {
