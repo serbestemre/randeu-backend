2ccfc17c59c56b87ad5dcef48771a9960535cc88
Emre Serbest <srbstemre@gmail.com>
2020-03-24 19:44:47 +0300

    ServiceMock is implemented, Service insertMany([]) Service deleteMany() DAL methods are implemented

--- src/dataAccess/Service.js
+++ src/dataAccess/Service.js
@@ -1,9 +1,15 @@
 const Service = require("../models/Service");
 
+exports.insertManySectorsDB = async services => Service.insertMany(services);
+
 exports.findServiceDB = async serviceName => Service.findOne(serviceName);
 
-exports.findServiceListByBusinessDB = async businessType => Service.find({ businessType });
+exports.findServiceListByBusinessDB = async businessType =>
+  Service.find({ businessType });
 
-exports.findServiceByIdDB = async searchedService => Service.findById(searchedService);
+exports.findServiceByIdDB = async searchedService =>
+  Service.findById(searchedService);
 
 exports.deleteServiceDB = async foundService => Service.deleteOne(foundService);
+
+exports.deleteManySectorsDB = async () => Service.deleteMany();
